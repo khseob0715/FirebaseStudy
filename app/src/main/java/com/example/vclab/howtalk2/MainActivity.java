94b47f38cfeae33f9965fb7f987a96137c094d3c
Han Seob Kim <khseob0715@gmail.com>
2018-04-30 00:26:18 +0900

    background push

--- app/src/main/java/com/example/vclab/howtalk2/MainActivity.java
+++ app/src/main/java/com/example/vclab/howtalk2/MainActivity.java
@@ -9,6 +9,12 @@ import android.view.MenuItem;
 
 import com.example.vclab.howtalk2.fragment.ChatFragment;
 import com.example.vclab.howtalk2.fragment.PeopleFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +41,17 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+        passPushTokenToServer();
 
     }
+    void passPushTokenToServer(){ // token을 생성해서 보내는 것
+        String uid =  FirebaseAuth.getInstance().getCurrentUser().getUid();
+        String token = FirebaseInstanceId.getInstance().getToken();       // token을 만드는 방법
+        // 데이터 베이스에 넘기기
+        // push token을 하는 방법은 hashmap 뿐임
+        Map<String, Object> map = new HashMap<>();
+        map.put("pushToken", token);
+        FirebaseDatabase.getInstance().getReference().child("users").child(uid).updateChildren(map); // setvalue하면 기존에 것들이 다 날라가버린다.
+    }
+
 }
